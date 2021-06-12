9964729c979fea2814606ec6f0a83c3f901144c8
B10856055 <B10856055@npust.edu.tw>
2021-06-12 21:22:21 +0800

    123

--- Start/Sign_in/User/U11_search_animal.php
+++ Start/Sign_in/User/U11_search_animal.php
@@ -1,7 +1,40 @@
+<?php
+    header("Content-Type: text/html; charset=utf8");
+    include('../../connect.php');
+    mysqli_query($connect,"SET NAMES 'UTF8'");
+    $email = parseurl($_COOKIE['user']);
+
+    function parseurl($url=""){
+        $url = rawurlencode(mb_convert_encoding($url, 'gb2312', 'utf-8'));
+        $a = array("%3A", "%2F", "%40");
+        $b = array(":", "/", "@");
+        $url = str_replace($a, $b, $url);
+        return $url;
+    }
+
+    $sql = "SELECT * FROM user WHERE email = '$email'";
+    $result = mysqli_query($connect,$sql);
+    for($i=1; $i <= mysqli_num_rows($result);$i++){
+        $text = mysqli_fetch_row($result);
+        $U_Name = $text[1];
+        $U_Email = $text[2];
+        $U_Password = $text[3];
+        $U_Sex = $text[4];
+        $U_ID_Number = $text[5];
+        $U_Phone = $text[6];
+        $U_Address = $text[7];
+        $U_Birthday = $text[8];
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <?php
+        if($email == ""){
+            echo "<meta http-equiv='refresh' content='0; url=../check_in.php'>";
+        }
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,13 +60,48 @@
             margin: 50px;
             font-size: 25px;
         }
+        .t1,
+        .t2,
+        .t3 {
+            font-size: 30px;
+            text-align: left;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        
+        .t4 {
+            text-align:center;
+            width: 100px;
+            height: 50px;
+            font-size: 30px;
+            background-color: burlywood;
+        }
+        
+        .t3 {
+            text-align:center;
+            width: 200px;
+            height: 50px;
+            font-size: 30px;
+        }
+        .t5{
+            width: 500px;
+            height: 50px;
+            font-size: 30px;
+        }
+        #test{
+            float: right;
+        }
     </style>
 </head>
 
 <body>
     <nav>
+        <a href="../check_out.php" id="test" style="text-decoration:none;">&nbsp;登出</a>
+        <a href="../../index.html" id="test">回首頁</a>
+        <?php
+            echo "<a id=test>HI $U_Name</a>";
+        ?>
         <img src="U01_user_UI/title.png">
-        <input type="button" value="回首頁" onclick="self.location.href='../../index.html'" id="in"><br>
         <img src="U01_user_UI/U11.png" alt="">
     </nav>
     <h1>請輸入查詢資料</h2>
@@ -47,7 +115,7 @@
             <tr>
                 <td><img src="U11_search_animal/shelter_animal-foota.png" style="width: 50%;"></td>
                 <td>
-                    <select style="float: left;margin-left: -300px;" id="option1">
+                    <select style="float: left;margin-left: -300px;" id="option">
                         <option id="one">請選擇 </option>
                         <script type="text/javascript">
                             function dog() {
@@ -111,7 +179,6 @@
                     <select style="float: left;margin-left: -300px;" id="option2">
                         <option>請選擇 </option>
                         <?php
-                            header("Content-Type: text/html; charset=utf8");
                             include('../../connect.php');
                             mysqli_query($connect,"SET NAMES 'UTF8'");
                             $sql = "SELECT `name` FROM `shelter`";
@@ -126,20 +193,148 @@
             </tr>
             <tr>
                 <td colspan="2">
-                    <input type="submit" value="查詢" style="font-size: 25px;margin-bottom: 50px;" onclick="print()">
+                    <input type="submit" value="查詢" style="font-size: 25px;margin-bottom: 50px;" onclick="test()">
                 </td>
             </tr>
         </table>
-    <script>
-        function print(){
-            alert(document.getElementById("option").value);
+    <script type="text/javascript">
+        function test(){
+            var animal = document.getElementById("option").value;
+            var shelter = document.getElementById("option2").value;
+            location.href="U11_search_animal.php?animal="+animal+"&shelter="+shelter;
         }
     </script>
-    <a href="U12_detail_animal.html"><img src="U11_search_animal/shelter_animal-1.png" style="width: 17%;"> </a>
-    <a href="U12_detail_animal.html"><img src="U11_search_animal/shelter_animal-2.png" style="width: 17%;"> </a>
-    <a href="U12_detail_animal.html"><img src="U11_search_animal/shelter_animal-3.png" style="width: 17%;"> </a>
-    <a href="U12_detail_animal.html"><img src="U11_search_animal/shelter_animal-4.png" style="width: 17%;"> </a>
-    <a href="U12_detail_animal.html"><img src="U11_search_animal/shelter_animal-5.png" style="width: 17%;"> </a>
+    <?php
+        include('../../connect.php');
+        mysqli_query($connect,"SET NAMES 'UTF8'");
+        $animal="請選擇";
+        $shelter="請選擇";
+        if($_GET['animal'] || $_GET['shelter']){
+            $animal = $_GET['animal'];
+            $shelter = $_GET['shelter'];
+        }
+        if($shelter != "請選擇" && $animal != "請選擇"){
+            $sql = "SELECT `id` FROM `shelter` WHERE `name` = '$shelter'";
+            $result = mysqli_query($connect,$sql);
+            if(mysqli_num_rows($result)>0){
+                $text = mysqli_fetch_row($result);
+                $sql2 = "SELECT * FROM `animal` WHERE `s_id` = '$text[0]' AND `species` = '$animal'";
+                $result2 = mysqli_query($connect,$sql2);
+                if(mysqli_num_rows($result2)>0){
+                    echo "<table border='1' class='t1' align='center'>";
+                    echo "<tr class='t4'><th>照片</th><th>類別</th><th>品種</th><th>性別</th><th>年齡</th><th>來自</th><th>詳細資料</th></tr>";
+                    if(mysqli_num_rows($result2)==1){
+                        $text = mysqli_fetch_row($result2);
+                        echo "<tr class='t2'>";
+                        echo "<td><img src='".$text[7]."'></td>";
+                        echo "<td class='t3'>$text[1]</td>";
+                        echo "<td class='t3'>$text[2]</td>";
+                        echo "<td class='t3'>$text[3]</td>";
+                        echo "<td class='t3'>$text[4]</td>";
+                        echo "<td class='t5'>$shelter</td>";
+                        echo "<td class='t3'><form action='' method='post'><input type='submit' name='submit' value='查看'></form></td>";
+                        echo "</tr>";
+                    }
+                    while ($row = mysqli_fetch_array($result2, mysqli_num_rows($result2))){
+                        echo "<tr class='t2'>";
+                        echo "<td><img src='".$row[7]."'></td>";
+                        echo "<td class='t3'>$row[1]</td>";
+                        echo "<td class='t3'>$row[2]</td>";
+                        echo "<td class='t3'>$row[3]</td>";
+                        echo "<td class='t3'>$row[4]</td>";
+                        echo "<td class='t5'>$shelter</td>";
+                        echo "<td class='t3'><form action='' method='post'><input type='submit' name='submit' value='查看'></form></td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }
+                else{
+                    echo "<script>alert('查無資料')</script>";
+                }
+            }
+        }
+        else if ($shelter != "請選擇" ){
+            $sql = "SELECT `id` FROM `shelter` WHERE `name` = '$shelter'";
+            $result = mysqli_query($connect,$sql);
+            if(mysqli_num_rows($result)>0){
+                $text = mysqli_fetch_row($result);
+                $sql2 = "SELECT * FROM `animal` WHERE `s_id` = '$text[0]'";
+                $result2 = mysqli_query($connect,$sql2);
+                if(mysqli_num_rows($result2)>0){
+                    echo "<table border='1' class='t1' align='center'>";
+                    echo "<tr class='t4'><th>照片</th><th>類別</th><th>品種</th><th>性別</th><th>年齡</th><th>來自</th><th>詳細資料</th></tr>";
+                    if(mysqli_num_rows($result2)==1){
+                        $text = mysqli_fetch_row($result2);
+                        echo "<tr class='t2'>";
+                        echo "<td><img src='".$text[7]."'></td>";
+                        echo "<td class='t3'>$text[1]</td>";
+                        echo "<td class='t3'>$text[2]</td>";
+                        echo "<td class='t3'>$text[3]</td>";
+                        echo "<td class='t3'>$text[4]</td>";
+                        echo "<td class='t5'>$shelter</td>";
+                        echo "<td class='t3'><form action='' method='post'><input type='submit' name='submit' value='查看'></form></td>";
+                        echo "</tr>";
+                    }
+                    while ($row = mysqli_fetch_array($result2, mysqli_num_rows($result2))){
+                        echo "<tr class='t2'>";
+                        echo "<td><img src='".$row[7]."'></td>";
+                        echo "<td class='t3'>$row[1]</td>";
+                        echo "<td class='t3'>$row[2]</td>";
+                        echo "<td class='t3'>$row[3]</td>";
+                        echo "<td class='t3'>$row[4]</td>";
+                        echo "<td class='t5'>$shelter</td>";
+                        echo "<td class='t3'><form action='' method='post'><input type='submit' name='submit' value='查看'></form></td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }
+                else{
+                    echo "<script>alert('查無資料')</script>";
+                }
+            }       
+        }
+        else if ($animal != "請選擇" ){
+            $sql = "SELECT * FROM `animal` WHERE `species` = '$animal'";
+            $result = mysqli_query($connect,$sql);
+            if(mysqli_num_rows($result)>0){
+                echo "<table border='1' class='t1' align='center'>";
+                echo "<tr class='t4'><th>照片</th><th>類別</th><th>品種</th><th>性別</th><th>年齡</th><th>來自</th><th>詳細資料</th></tr>";
+                if(mysqli_num_rows($result)==1){
+                    $text = mysqli_fetch_row($result);
+                    $sql2 = "SELECT `name` FROM `shelter` WHERE `id` = '$text[9]'";
+                    $result2 = mysqli_query($connect,$sql2);
+                    $text2 = mysqli_fetch_row($result2);
+                    echo "<tr class='t2'>";
+                    echo "<td><img src='".$text[7]."'></td>";
+                    echo "<td class='t3'>$text[1]</td>";
+                    echo "<td class='t3'>$text[2]</td>";
+                    echo "<td class='t3'>$text[3]</td>";
+                    echo "<td class='t3'>$text[4]</td>";
+                    echo "<td class='t5'>$text2[0]</td>";
+                    echo "<td class='t3'><form action='' method='post'><input type='submit' name='submit' value='查看'></form></td>";
+                    echo "</tr>";
+                }
+                while ($row = mysqli_fetch_array($result, mysqli_num_rows($result))){
+                    $sql2 = "SELECT `name` FROM `shelter` WHERE `id` = '$text[9]'";
+                    $result2 = mysqli_query($connect,$sql2);
+                    $text2 = mysqli_fetch_row($result2);
+                    echo "<tr class='t2'>";
+                    echo "<td><img src='".$row[7]."'></td>";
+                    echo "<td class='t3'>$row[1]</td>";
+                    echo "<td class='t3'>$row[2]</td>";
+                    echo "<td class='t3'>$row[3]</td>";
+                    echo "<td class='t3'>$row[4]</td>";
+                    echo "<td class='t5'>$text2[0]</td>";
+                    echo "<td class='t3'><form action='' method='post'><input type='submit' name='submit' value='查看'></form></td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            else{
+                echo "<script>alert('查無資料')</script>";
+            }     
+        }
+    ?>
 </body>
 
 </html>
