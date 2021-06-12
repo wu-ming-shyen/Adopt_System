4c3fc61344dc8896952fe5ff66bfe8c1670af138
wu-ming-shyen <Wu.Ming.Shyen@gmail.com>
2021-06-13 04:16:26 +0800

    M11_data_manager.php:

---
+++ Start/Sign_in/Manager/M11_data_manager.php
@@ -0,0 +1,152 @@
+<?php
+    header("Content-Type: text/html; charset=utf8");
+    include('../../connect.php');
+    mysqli_query($connect,"SET NAMES 'UTF8'");
+    $email = parseurl($_COOKIE['user']);
+
+    function parseurl($url=""){
+        $url = rawurlencode(mb_convert_encoding($url, 'gb2312', 'utf-8'));
+        $a = array("%3A", "%2F", "%40");
+        $b = array(":", "/", "@");
+        $url = str_replace($a, $b, $url);
+        return $url;
+    }
+    
+    $sql = "SELECT * FROM `manager` WHERE `email` = '$email'";
+    $result = mysqli_query($connect,$sql);
+    for($i=1; $i <= mysqli_num_rows($result);$i++){
+        $text = mysqli_fetch_row($result);
+        $M_Name = $text[1];
+        $M_Email = $text[2];
+        $M_Password = $text[3];
+        $M_Birthday = $text[4];
+        $M_Phone = $text[5];
+        $M_ID_Number = $text[6];
+        $M_Sex = $text[7];
+        $M_Address = $text[8];
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+        if($email == ""){
+            echo "<meta http-equiv='refresh' content='0; url=../check_in.php'>";
+        }
+    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>貓狗領養系統</title>
+    <style>
+        body {
+            background-color: papayawhip;
+        }
+        
+        nav {
+            text-align: center;
+        }
+        
+        main {
+            text-align: center;
+        }
+        
+        table,
+        th,
+        td {
+            font-size: 30px;
+            text-align: left;
+            border: 2px solid black;
+            border-collapse: collapse;
+        }
+        
+        th {
+            width: 200px;
+            height: 50px;
+            font-size: 30px;
+            background-color: burlywood;
+        }
+        
+        td {
+            width: 500px;
+            height: 50px;
+            font-size: 30px;
+        }
+        
+        input {
+            width: 99.25%;
+            height: 95%;
+            margin-left: -2px;
+            font-size: 30px;
+        }
+        
+        input[type="submit"] {
+            margin-top: 50px;
+            font-size: 30px;
+            width: 250px;
+        }
+        
+        #test{
+            float: right;
+            text-decoration:none;
+        }
+    </style>
+</head>
+
+<body>
+    <nav>
+        <a href="../check_out.php" id="test" style="text-decoration:none;">&nbsp;登出</a>
+        <a href="U01_user_UI.html" id="test">回首頁</a>
+        <?php
+            echo "<a id=test>HI $M_Name</a>";
+        ?>
+        <image src="M01_manager_UI/title.png" alt=""><br>
+        <image src="M01_manager_UI/M11.png" alt=""></image>
+    </nav>
+    <main>
+        <form action="update.php" method="POST">
+        <?php echo "<input type='hidden' value='$email' name='email'>"; ?>
+        <table align="center">
+            <tr>
+                <th>管理員名稱</th>
+                <?php echo "<td><input type='text' value='$M_Name' name=M_Name></td>"; ?>
+            </tr>
+            <tr>
+                <th>電子郵件</th>
+                <td style="background-color: lightyellow;">
+                    <?php echo "$M_Email"; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>密碼</th>
+                <?php echo "<td><input type='text' value='$M_Password' name=M_Password></td>"; ?>
+            </tr>
+            <tr>
+                <th>生日</th>
+                <?php echo "<td><input type='text' value='$M_Birthday' name=M_Birthday></td>"; ?>
+            </tr>
+            <tr>
+                <th>電話</th>
+                <?php echo "<td><input type='text' value='$M_Phone' name=M_Phone></td>"; ?>
+            </tr>
+            <tr>
+                <th>性別</th>
+                <?php echo "<td><input type='text' value='$M_Sex' name=M_Sex></td>"; ?>
+            </tr>
+            <tr>
+                <th>戶籍</th>
+                <?php echo "<td><input type='text' value='$M_Address' name=M_Address></td>"; ?>
+            </tr>
+            <tr>
+                <th>身份證字號</th>
+                <td style="background-color: lightyellow;">
+                    <?php echo "$M_ID_Number"; ?>
+                </td>
+            </tr>
+        </table>
+        <button><b>送出</b></button>
+    </main>
+</body>
+
+</html>
---
+++ Start/Sign_in/Manager/update.php
@@ -0,0 +1,25 @@
+<?php
+    $email = $_POST['email'];
+    $M_Name = $_POST['M_Name'];
+    $M_Password = $_POST['M_Password'];
+    $M_Birthday = $_POST['M_Birthday'];
+    $M_Phone = $_POST['M_Phone'];
+    $M_Sex = $_POST['M_Sex'];
+    $M_Address = $_POST['M_Address'];
+
+    include('../../connect.php');
+    mysqli_query($connect,"SET NAMES 'UTF8'");
+
+    $sql = "UPDATE `manager` SET `name` = '$M_Name', `password` = '$M_Password', `birthday` = '$M_Birthday', `Phone` = '$M_Phone', `sex` = '$M_Sex', `address` = '$M_Address' WHERE `manager`.`email` = '$email';";
+    $result = mysqli_query($connect,$sql);
+
+    if(!$result){
+        echo "script>alert('ERROR');</script>";
+        header("refresh:0;url=M11_data_manager.php");
+    }else{
+        echo "<script>alert('修改成功');</script>";
+        header("refresh:0;url=M11_data_manager.php");
+    }
+
+    mysqli_close($connect);
+?>
